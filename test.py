88582797ab06bf2f7b21f82dcfcb0b0e0809a768
PhamVietHaK64 <vitga5c@gmail.com>
2022-05-10 18:15:07 +0700

    commit

--- test.py
+++ test.py
@@ -10,6 +10,80 @@ from ReadFile import maps5, maps7, maps10, maps15, maps20, maps30
 outputFile = 'output/output.txt'
 
 
+
+
+class MyApp(QtWidgets.QMainWindow):
+    __maps = [maps5, maps7, maps10, maps15, maps20, maps30]
+    __size = 0
+    __map = 0
+    __slitherlink_size = 0
+
+    def __init__(self):
+        super(MyApp, self).__init__()
+        """uic.loadUi('ui.ui', self)
+        self.setWindowTitle("Slitherlink")
+        self.create_combobox(0)
+        self.create_map(0)
+        self.input_size.currentIndexChanged.connect(self.create_combobox)
+        self.input_map.currentIndexChanged.connect(self.create_map)
+        self.solveButton.clicked.connect(self.solver)
+        """
+
+        #đây là đoạn test Qpainter nếu ko chạy đoạn ẩn bên trên thì đc 
+        #nhưng chạy cả bên trên thì cái trên nó che mất 
+        self.repaint()
+        self.show()
+
+    def create_combobox(self, index):
+        self.__size = index
+        self.input_map.clear()
+        length = len(self.__maps[self.__size])
+        for i in range(length):
+            self.input_map.addItem(str(i + 1))
+    def create_map(self, index):
+        h = self.height()
+        self.__map = index
+        size = len(self.__maps[self.__size][self.__map])
+    
+        self.__slitherlink_size = size
+        self.map.setRowCount(size)
+        self.map.setColumnCount(size)
+        self.map.resize(size*20, size*20)
+        self.map.setGeometry(0, 0, 20* (size + 1), 20 * (size + 1))
+        self.map.resizeColumnsToContents()
+        self.map.verticalHeader().setVisible(False)
+        self.map.horizontalHeader().setVisible(False)
+        self.setStyleSheet("background-color: #C6E2FF")
+        for i in range(size):
+            for j in range(size):
+                item = self.__maps[self.__size][self.__map][i][j]
+                item = str(item)
+                item = QtWidgets.QTableWidgetItem(item)
+                self.map.setItem(i, j, item)
+                self.map.item(i, j).setTextAlignment(Qt.AlignCenter)
+                if item.text():
+                    self.map.item(i, j).setBackground(Qt.white)
+    
+    def solver(self):
+        run_all_test()
+        result = solve(self.__maps[self.__size][self.__map])
+        self.clause.setText("Clause: " + str(result['clauses']))
+        self.variable.setText('Variable: ' + str(result['variables']))
+        self.time.setText("Time: %.5f s" % (result['time']))
+
+
+        self.repaint()
+        
+        
+     
+    def paintEvent(self, event):
+
+        qp = QPainter()
+        qp.begin(self)
+        qp.setPen(Qt.red)
+        qp.drawRect(0, 0, 30, 30)
+        qp.end()
+
 def read_result(result, file):
     sol = result["result"]
     time = result['time']
@@ -59,63 +133,6 @@ def run_all_test():
         for m in mapi:
             read_result(solve(m), file)
     file.close()
-
-
-class MyApp(QtWidgets.QMainWindow):
-    __maps = [maps5, maps7, maps10, maps15, maps20, maps30]
-    __size = 0
-    __map = 0
-    __slitherlink_size = 0
-
-    def __init__(self):
-        super(MyApp, self).__init__()
-        uic.loadUi('ui.ui', self)
-        self.setWindowTitle("Slitherlink")
-        self.input_size.currentIndexChanged.connect(self.create_combobox)
-        self.input_map.currentIndexChanged.connect(self.create_map)
-
-        self.show()
-
-    def create_combobox(self, index):
-        self.__size = index
-        self.input_map.clear()
-        length = len(self.__maps[self.__size])
-        for i in range(length):
-            self.input_map.addItem(str(i + 1))
-    def create_map(self, index):
-        h = self.height()
-        self.map.setRowCount(0)
-        self.__map = index
-        size = len(self.__maps[self.__size][self.__map])
-        print(size)
-        system("pause")
-        self.__slitherlink_size = size
-        self.map.setRowCount(size)
-        self.map.setColumnCount(size)
-        self.map.setGeometry(0, 0, 20* size, 20 * size)
-        self.map.resize(20 * size, 20 * size)
-        self.setStyleSheet("background-color: #C6E2FF")
-        for i in range(size):
-            for j in range(size):
-                item = self.__maps[self.__size][self.__map][i][j]
-                item = str(item)
-                item = QtWidgets.QTableWidgetItem(item)
-                self.map.setItem(i, j, item)
-                self.map.item(i, j).setTextAlignment(Qt.AlignCenter)
-                if item.text():
-                    self.map.item(i, j).setBackground(Qt.lightGray)
-    
-def solver(self):
-        result = dict()
-        self.load_result(result['result'])
-        self.clause.setText("Clause: " + str(result['clauses']))
-        self.variable.setText('Variable: ' + str(result['variables']))
-        self.time.setText("Time: %.5f s" % (result['time']))
-def load_result(self, result):
-        for i in range(self.__slitherlink_size):
-            for j in range(self.__slitherlink_size):
-                item = result[i][j]
-                self.map.item(i, j).setText(str(item))
 
 app = QApplication(sys.argv)
 UIWindow = MyApp()
